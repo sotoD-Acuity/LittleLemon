d71017e66c43646f1288c3f343afefe24f606dc6
nephillim13 <diegoasoto13@outlook.com>
2024-12-17 22:29:03 -0700

    Added Booking & Menu models.

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from restaurant import models
+
 
 # Register your models here.
+admin.site.register(models.Menu)
+admin.site.register(models.Booking)
--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core import validators
 
 # Create your models here.
+class Booking(models.Model):
+    id = models.IntegerField(
+        primary_key=True,
+        validators=[validators.MinValueValidator(0)]
+    )
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(
+        validators= [validators.MinValueValidator(1), validators.MaxValueValidator(999999)]
+    )
+    booking_date = models.DateTimeField(unique_for_date=True)
+    
+class Menu(models.Model):
+    id = models.IntegerField(
+        primary_key=True,
+        validators= [validators.MinValueValidator(0), validators.MaxValueValidator(99999)]
+    )
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    inventory = models.IntegerField(
+        validators=[validators.MinValueValidator(0), validators.MaxValueValidator(99999)]
+    )
+    
